12e5a55db6747cff7a05fdfbb41ffd12c04386b4
johann2912 <johanngomez02@gmail.com>
2025-02-15 22:54:11 -0500

    Remove unused log import and clean up code structure

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"menu360/config"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-
 	config.LoadEnv()
 
 	config.ConnectDB()
--- config/env.go
+++ config/env.go
@@ -6,7 +6,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
 func LoadEnv() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
